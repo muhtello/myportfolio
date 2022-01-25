a4864dab289fc6290711793af3fa1471a248d47c
muhanad212 <muhtello@gmail.com>
2022-01-25 11:36:47 +0100

    dele pic betterLife

--- src/components/display/Game/gameProfile/picProfileJS/BetterLife.js
+++ src/components/display/Game/gameProfile/picProfileJS/BetterLife.js
@@ -4,17 +4,8 @@ import ABetterLifeSea2 from "../../../../../picdata/ToABetterLife/ABetterLifeSea
 
 export const BETTERLIFE = [
   
-<<<<<<< HEAD
   {
     idDiscription: 1,
-=======
-  //{
-    //idDiscription: 1,
-    //pic: ToABetterLifeScreenStart
-  //},
-  {
-    idDiscription: 2,
->>>>>>> 2538a472e3acc7e5c6b6766986a1c690a37149f2
     pic: ToABetterLifeboat
   },
   {
